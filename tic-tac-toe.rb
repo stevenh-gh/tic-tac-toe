ee3719fd002030f1867ab24a3ef7c8c51f67912e
steven huynh <steven.h@live.com>
2020-10-29 16:20:39 -0700

    spacing after restart game prompt

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -12,7 +12,7 @@ def play_ttt
     good_input = false
     input = nil
     until good_input
-      print 'Restart game? (Y/N)'
+      print 'Restart game? (Y/N): '
       input = gets.chomp
       good_input = true if input.downcase == 'y' || input.downcase == 'n'
     end
